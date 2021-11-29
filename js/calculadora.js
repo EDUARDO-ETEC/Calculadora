fa81949abb950ee2aef59c0ae3d6a61c5d9a6f39
PoKsRammerfell <78971152+EDUARDO-ETEC@users.noreply.github.com>
2021-11-29 16:41:14 -0300

    Add files via upload

---
+++ js/calculadora.js
@@ -0,0 +1,30 @@
+function insere(numero) {
+
+    var valor = document.getElementById('resultado').innerHTML;
+  
+    document.getElementById('resultado').innerHTML = valor + numero;
+  
+  }
+  
+  function limpar(){
+      document.getElementById('resultado').innerHTML = "";
+  }
+  
+  function voltar(){
+      var resultado = document.getElementById('resultado').innerHTML;
+      document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.lenght -1);
+  }
+
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado){
+      document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else{
+      
+      document.getElementById('resultado').innerHTML = "nada...";
+
+    }
+  }  
+
